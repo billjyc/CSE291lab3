5eb2da4de9b8ecc55df26770ea0fefe9f5c7ea81
billjyc <jiayicong@vip.qq.com>
2016-05-27 20:23:25 -0700

    first commit

---
+++ src/WordCount.java
@@ -0,0 +1,79 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
+
+/**
+ * Created by billjyc on 5/27/16.
+ */
+public class WordCount {
+
+    public static class TokenizerMapper
+            extends org.apache.hadoop.mapreduce.Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String[] iters = value.toString().split(" ");
+            for(int i = 0; i < iters.length - 1; i++) {
+                word.set(iters[i] + " " + iters[i + 1]);
+                context.write(word, one);
+            }
+//            StringTokenizer itr = new StringTokenizer(value.toString());
+//            while (itr.hasMoreTokens()) {
+//                word.set(itr.nextToken());
+//                context.write(word, one);
+//            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends org.apache.hadoop.mapreduce.Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: wordcount <in> [<in>...] <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(WordCount.IntSumReducer.class);
+        job.setReducerClass(WordCount.IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(4);
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        org.apache.hadoop.mapreduce.lib.output.FileOutputFormat.setOutputPath(job,
+                new Path(otherArgs[otherArgs.length - 1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
